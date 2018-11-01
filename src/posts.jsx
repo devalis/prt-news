808619a2f2d894c071af0841f376441b265bbe39
Sofia Alysandratou <sofia.alys@hitsa.local>
2018-11-01 16:20:21 +0200

    right order

--- src/posts.jsx
+++ src/posts.jsx
@@ -1,27 +1,27 @@
 export const posts = [
   {
-    id: 1,
+    id: 9,
     date: '7 May 2018',
     fbimg: require("./img/prt180507.jpg"),
     description: 'Πανελλήνιο Πρωτάθλημα 10000μ Νέων γυναικών σήμερα στα Μέγαρα! 1η θέση με σπουδαίο ατομικό ρεκόρ 36.43.56 για την 21χρονη αθλήτρια μας Μοσχούλα Μανούσου-Παρασκευοπούλου! Συγχαρητήρια και καλή συνέχεια από όλο το PRT',
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1788437731201986'
   },
   {
-    id: 2,
+    id: 8,
     date: '13 May 2018',
     fbimg: require("./img/prt180513.jpg"),
     description: 'Άλλη μια νίκη για τον ταλαντούχο και πολλά υποσχόμενο (14χρονο) Χ.Βαρέλη! Συγχαρητήρια στον αθλητή μας!',
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1794504827261943'
   },
   {
-    id: 3,
+    id: 7,
     date: '19 June 2018',
     fbimg: require("./img/prt180619.jpg"),
     description: 'Πολλά συγχαρητήρια στην αθλήτρια του PRT και Γ.Σ. Κηφισιάς, Ελένη Ευθυμίου που έκανε φοβερή κούρσα στο Πανελλήνιο Πρωτάθλημα Νεανίδων το περασμένο Σαββατοκύριακο στα Τρίκαλα κατακτώντας την 2η θέση στα 3000μ στηπλ με 11.34 (αν και κορασίδα)! Καλή επιτυχία την Κυριακή στο πρωτάθλημα της κατηγορίας της!',
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1834930756552683'
   },
   {
-    id: 4,
+    id: 6,
     date: '26 June 2018',
     fbimg: require("./img/prt180626.jpg"),
     description: 'Το καθημερινό πρόγραμμα ενός αθλητή αποτελείται απο πολλές ώρες εξαντλητικής προπόνησης. Σύμμαχος μας για καλύτερη αντοχή το Redoferro Max. Ευχαριστούμε την εταιρεία Heremco Pharmaceuticals Ltd. για την βοήθεια που παρέχει στον αθλητη μας (Παλαιστίνιο πρόσφυγα, αθλητή 400μ-800μ) Mohammed A Alrahban',
@@ -35,28 +35,28 @@ export const posts = [
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1847543068624785'
   },
   {
-    id: 6,
+    id: 4,
     date: '16 July 2018',
     fbimg: require("./img/prt180716.jpg"),
     description: 'Πανελλήνιο Πρωτάθλημα Α/Γ: 2 χρυσά και 2 χάλκινα μετάλλια για τους αθλητές του PRT στο Πανελλήνιο Πρωτάθλημα που διεξήχθη στο Πελοποννησιακό Στάδιο (Πάτρα).',
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1875299682515790'
   },
   {
-    id: 7,
+    id: 3,
     date: '20 July 2018',
     fbimg: require("./img/prt180720.jpg"),
     description: 'Χρυσό μετάλλιο για την Κωνσταντίνα Γιαννοπούλου στα 800μ με 2.04.65 (καλύτερη φετινή επίδοση) στους Βαλκανικούς αγώνες Α/Γ πριν λίγο στη Στάρα Ζαγόρα! Συγχαρητήρια!!!',
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1882014235177668'
   },
   {
-    id: 8,
+    id: 2,
     date: '30 July 2018',
     fbimg: require("./img/prt180730.jpg"),
     description: 'Τέλος σεζόν με το Πανελλήνιο πρωτάθλημα Νέων για Κοτίτσα, Μανούσου Παρασκευοπούλου και Μαστιχιάδη με μετάλλια και ατομικά ρεκόρ! Συγχαρητήρια από όλο το PRT!', 
     fbpost: 'https://www.facebook.com/PrtPapachristosRunningTeam/posts/1897119957000429'
   },
   {
-    id: 9,
+    id: 1,
     date: '12 August 2018',
     fbimg: require("./img/prt180812.jpg"),
     description: 'Πολλά συγχαρητήρια στον αθλητή μας Κώστα Γκελαούζο που έκανε πολύ καλή εμφάνιση στον μαραθώνιο του Ευρωπαϊκού Πρωταθλήματος, όπου τερμάτισε στην 44η θέση με επίδοση κοντά στο ατομικό του ρεκορ 2.22.24!', 
